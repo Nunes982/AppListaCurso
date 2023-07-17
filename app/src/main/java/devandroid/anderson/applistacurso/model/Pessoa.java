b91e447d32d44eeb07cd0017c5aeec0e4e1d6be3
Nunes1982 <ansso06061982@gmail.com>
2023-07-17 16:20:59 -0300

    Uso dos métodos Get and Set

--- app/src/main/java/devandroid/anderson/applistacurso/model/Pessoa.java
+++ app/src/main/java/devandroid/anderson/applistacurso/model/Pessoa.java
@@ -2,11 +2,52 @@ package devandroid.anderson.applistacurso.model;
 
 public class Pessoa {
 
-    //Construtor
+    //Atributos - Objetos - Molde - Modelo - Template
 
-    public Pessoa(){
+    private String primeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+    private String telefoneContato;
 
-        
+    //Metodos de acesso - Gets And Setters
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
     }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
+    //Construtor
+    public Pessoa() {
+
+    }
+
+
 
 }
--- app/src/main/java/devandroid/anderson/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/anderson/applistacurso/view/MainActivity.java
@@ -10,6 +10,10 @@ import devandroid.anderson.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
 
 
     @Override
@@ -18,7 +22,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        //Atribuir conteúdo, dados, valores para o Objeto
+        //Conformnee o seu MODELO, TEMPLATE
+        pessoa.setPrimeiroNome("Anderson");
+        pessoa.setSobreNome("Nunes");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("(11) 99223-3554");
 
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Renata");
+        outraPessoa.setSobreNome("Candido");
+        outraPessoa.setCursoDesejado("Psicologia");
+        outraPessoa.setTelefoneContato("(11) 91234-5678");
+
+
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso Desejado ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de Contato ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+        int parada = 0;
 
     }
 }
